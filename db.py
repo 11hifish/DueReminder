636f10dd901fc1f959edc803a5ea71c034710273
haohanshi <haohans@andrew.cmu.edu>
2016-09-16 22:29:18 -0400

    modified db.py

--- db.py
+++ db.py
@@ -9,6 +9,7 @@ c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
 conn.commit()
+print("1233455667")
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
