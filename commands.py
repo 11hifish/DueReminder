b4909366fe293867574f69b3ea3c0fc00a8757b8
Xingsheng Wang <xingshengwang@XingshengdeMBP.wv.cc.cmu.edu>
2016-09-17 02:56:46 -0400

    commands

--- commands.py
+++ commands.py
@@ -15,36 +15,52 @@ def getuserInfo(andrewid):
         return (userdict[andrewid])
     else:
         return "No user found!"
-        
-def addcourse(andrewid, coursecode):
+def studentRole(andrewid, coursecode):
+    for x in userdict[andrewid][1]:
+        if x[0] == coursecode:
+            return x[-1]
+def addCourse(andrewid, coursecode,role):
     if len(userdict[andrewid]) == 1:
-        userdict[andrewid].append([(coursecode, coursedict[coursecode])])
+        userdict[andrewid].append([(coursecode, coursedict[coursecode],role)])
     else:
-        userdict[andrewid][1].append((coursecode, coursedict[coursecode]))
+        userdict[andrewid][1].append((coursecode, coursedict[coursecode],role))
     return (userdict[andrewid])
     
-def removeCourse(andrewid, coursecode):
-    userdict[andrewid][1].remove((coursecode, coursedict[coursecode]))
+def removeCourse(andrewid, coursecode, role):
+    userdict[andrewid][1].remove((coursecode, coursedict[coursecode],role))
     return (userdict[andrewid])
 
 duedict = dict()
 def addDue(coursecode, dueDate, homeworkName, handinType):
-    duedict[coursecode] += [(dueDate, homeworkName, handinType)]
+    if not coursecode in duedict:
+        duedict[coursecode] =[(dueDate, homeworkName, handinType)]
+    else:
+        duedict[coursecode].append((dueDate, homeworkName, handinType))
 
-def getduedate(coursecode):
+def getDueDate(coursecode):
     if coursecode in duedict:
         return duedict[coursecode]
     else:
-        return "No Course found!"
+        return "No Course Found"
+
+def removeDueDate(coursecode, dueDate, homeworkName, handinType):
+    duedict[coursecode].remove((dueDate, homeworkName, handinType))
+    
     
 addUser("xingshew","Xingsheng Wang")
 print(getuserInfo("xingshew"))
-addcourse("xingshew",15112)
+addCourse("xingshew",15112,"student")
 print(getuserInfo("xingshew"))
-addcourse("xingshew",21259)
+addCourse("xingshew",21259,"student")
 print(getuserInfo("xingshew"))
 addDue(15112, "Sept.23rd","Written1", "recitation")
-print(getduedate("xingshew"))
+addDue(15112, "Sept.24rd","Written2", "recitation")
+removeCourse("xingshew",21259,"student")
+print(getuserInfo("xingshew"))
+print(getDueDate(15112))
+removeDueDate(15112, "Sept.23rd","Written1", "recitation")
+print(getDueDate(15112))
+print(studentRole("xingshew",15112))
 
      
     
